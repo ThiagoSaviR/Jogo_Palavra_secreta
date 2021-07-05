f5a9f952dd79e9b73def0ef971708ea51d909731
ThiagoSaviR <thiagosavir@gmail.com>
2021-07-05 12:21:16 -0300

    adiciona jogo

---
+++ Jogo_Palavra_Secreta.py
@@ -0,0 +1,30 @@
+"""""
+jogo: Palavra secreta
+"""""
+
+secreta = 'lasanha'
+dig = []
+
+while True:
+    letra_dig = input('digite uma letra: ')
+
+    if len(letra_dig) > 1:
+        print('Ahh, não pode roubar... Digite uma letra por vez.')
+        continue
+    dig.append(letra_dig)
+
+    if letra_dig in secreta:
+        print('Aeee, acertou!!!')
+    else:
+        print('Ahh, errou...')
+        dig.pop()
+
+    letra_em_secreta = ''
+
+    for i in secreta:
+        if i in dig:
+            letra_em_secreta += i
+        else:
+            letra_em_secreta += '_'
+
+    print(f'A palavra secreta está assim: {letra_em_secreta}')
